4fc231f63000ddfa11c9cea2ae1a596501d63148
Md Lutfor Tahman Shifat <shifat151@gmail.com>
2020-01-06 15:21:06 +0600

    base files location changed and seetings file updated

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'producthunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['producthunt/templates'],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- producthunt/urls.py
+++ producthunt/urls.py
@@ -10,6 +10,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
-
-
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
