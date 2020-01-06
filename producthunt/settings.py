9f60ea5a5c4c39906f6920657f20228838726107
Md Lutfor Tahman Shifat <shifat151@gmail.com>
2020-01-06 15:53:55 +0600

    Database changed uninstall dependecies and migration done for hosting into python anywhere

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -81,15 +81,19 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'producthuntdb',
-        'USER':'postgres',
-        'PASSWORD':'besimple0',
-        'HOST':'localhost',
-        'PORT':'5432',
-
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'producthuntdb',
+    #     'USER':'postgres',
+    #     'PASSWORD':'besimple0',
+    #     'HOST':'localhost',
+    #     'PORT':'5432',
+
+
+    # }
 }
 
 
